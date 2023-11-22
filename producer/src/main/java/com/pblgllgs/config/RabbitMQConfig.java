d32e06f19653f8a12fd6f3371a1e99508ff8b2d8
pbl.gllgs <pbl.gllgs@gmail.com>
2023-11-22 15:01:59 -0300

    multi message type

---
+++ producer/src/main/java/com/pblgllgs/config/RabbitMQConfig.java
@@ -0,0 +1,60 @@
+package com.pblgllgs.config;
+/*
+ *
+ * @author pblgl
+ * Created on 21-11-2023
+ *
+ */
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${q.invoice}")
+    private String queue;
+
+
+    @Value("${x.invoice}")
+    private String exchange;
+
+    @Bean
+    public Queue newQueue() {
+        return new Queue(queue);
+    }
+
+    @Bean
+    public FanoutExchange exchange() {
+        return new FanoutExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding(Queue newQueue, FanoutExchange exchange) {
+        return BindingBuilder.bind(newQueue).to(exchange);
+    }
+
+    @Bean
+    Jackson2JsonMessageConverter jsonMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+
+
+    @Bean
+    RabbitTemplate rabbitTemplate(Jackson2JsonMessageConverter converter, ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter( jsonMessageConverter());
+        return template;
+    }
+
+}
---
+++ producer/src/main/java/com/pblgllgs/data/DataLoader.java
@@ -0,0 +1,44 @@
+package com.pblgllgs.data;
+/*
+ *
+ * @author pblgl
+ * Created on 21-11-2023
+ *
+ */
+
+import com.pblgllgs.entities.InvoiceCreatedMessage;
+import com.pblgllgs.entities.InvoicePaidMessage;
+import com.pblgllgs.producer.InvoiceProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DataLoader implements CommandLineRunner {
+    private final InvoiceProducer producer;
+
+    @Override
+    public void run(String... args) {
+        var randomInvoiceNumber = "INV-" + ThreadLocalRandom.current().nextInt(100, 200);
+        InvoiceCreatedMessage invoiceCreatedMessage = new InvoiceCreatedMessage(
+                152.26,
+                LocalDate.now().minusDays(2),
+                "USD",
+                randomInvoiceNumber);
+        producer.sendInvoice(invoiceCreatedMessage);
+        randomInvoiceNumber = "INV-" + ThreadLocalRandom.current().nextInt(200, 300);
+        var randomPaymentNumber = "PAY-" + ThreadLocalRandom.current().nextInt(800, 1000);
+        var invoicePaidMessage = new InvoicePaidMessage(
+                randomInvoiceNumber,
+                LocalDate.now(),
+                randomPaymentNumber);
+        producer.sendInvoice(invoicePaidMessage);
+        log.info("DONE!!");
+    }
+}
